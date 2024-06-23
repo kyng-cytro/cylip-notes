d281794391c0d9e4be19669fca4fde7d3454190a
Kyng Cytro <johndibashi2001@gamil.com>
2024-06-23 08:23:42 +0100

    chore: remove unnecessary padding

--- layouts/landing.vue
+++ layouts/landing.vue
@@ -4,7 +4,7 @@
     class="flex min-h-screen flex-col justify-between bg-background text-foreground"
   >
     <LandingHeader />
-    <main class="py-8">
+    <main>
       <NuxtPage />
     </main>
     <LandingFooter />
