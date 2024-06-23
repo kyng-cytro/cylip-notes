536bcd249a18273a3a91e5fa3a4975cc8df0e244
kyng-cytro <johndibashi2001@gmail.com>
2024-06-23 08:14:39 +0100

    feat: add pricing page

---
+++ components/landing/PriceCard.vue
@@ -0,0 +1,68 @@
+<script setup lang="ts">
+defineProps<{
+  price: string;
+  onetime?: boolean;
+  title: string;
+  perks: string[];
+  extras?: string[];
+  button: {
+    text: string;
+    to: string;
+    variant: "default" | "outline";
+  };
+  description: string;
+}>();
+</script>
+<template>
+  <Card class="flex h-full w-full flex-col">
+    <CardHeader>
+      <CardTitle class="text-2xl font-semibold leading-8">{{
+        title
+      }}</CardTitle>
+      <CardDescription class="text-md mt-3">{{ description }}</CardDescription>
+      <CardDescription class="text-md mt-4 gap-x-1 text-primary">
+        <span class="text-4xl font-bold tracking-tight">${{ price }}</span>
+        <span class="text-sm font-semibold leading-6" v-if="!onetime"
+          >/month</span
+        >
+      </CardDescription>
+    </CardHeader>
+    <CardContent>
+      <Button
+        class="w-full font-semibold"
+        :variant="button.variant"
+        size="lg"
+        >{{ button.text }}</Button
+      >
+    </CardContent>
+    <CardFooter
+      class="flex flex-col items-start justify-between gap-y-3 lg:h-52"
+    >
+      <ul role="list" class="mt-3 space-y-3 text-sm leading-6 lg:mt-6">
+        <li class="flex gap-x-3" :key="perk" v-for="perk in perks">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            class="h-6 w-5 flex-none text-primary"
+            aria-hidden="true"
+          >
+            <path
+              fill="currentColor"
+              fill-rule="evenodd"
+              d="M19.916 4.626a.75.75 0 0 1 .208 1.04l-9 13.5a.75.75 0 0 1-1.154.114l-6-6a.75.75 0 0 1 1.06-1.06l5.353 5.353l8.493-12.739a.75.75 0 0 1 1.04-.208Z"
+              clip-rule="evenodd"
+            ></path></svg
+          >{{ perk }}
+        </li>
+      </ul>
+      <ul>
+        <li
+          class="flex gap-x-3 text-xs text-muted-foreground"
+          v-for="extra in extras"
+        >
+          {{ extra }}
+        </li>
+      </ul>
+    </CardFooter>
+  </Card>
+</template>
--- pages/pricing.vue
+++ pages/pricing.vue
@@ -4,5 +4,72 @@ definePageMeta({
 });
 </script>
 <template>
-  <div>Pricing</div>
+  <div class="sm:-mt-16">
+    <div class="flex items-center justify-center">
+      <div class="relative isolate w-full max-w-6xl px-6 py-14 lg:px-8">
+        <!-- Text -->
+        <div class="text-center">
+          <h1 class="text-4xl font-bold tracking-tight sm:text-5xl">
+            Pricing Is Simple, As<br />
+            We Put You First
+          </h1>
+          <p class="mt-6 text-lg leading-8 text-muted-foreground">
+            Choose from <span class="font-semibold">Free</span> basics,
+            <span class="font-semibold">Pro-level</span> perks, or top up on AI
+            <span class="font-semibold">Tokens</span> as you go.
+          </p>
+        </div>
+        <!-- Pricing Table -->
+        <div
+          class="mt-10 flex flex-col items-center gap-y-6 sm:justify-center sm:gap-x-6 lg:flex-row"
+        >
+          <LandingPriceCard
+            price="0"
+            title="Freeloader"
+            :perks="[
+              'Dark theme',
+              'Unlimited notes',
+              '100 AI tokens per month (enough to impress your cat)',
+            ]"
+            :extras="['Keep in mind this means you\'re a brokie']"
+            :button="{
+              text: 'Sign Up for Free',
+              to: '/dashboard',
+              variant: 'outline',
+            }"
+            description="All the basics, for free."
+          />
+          <LandingPriceCard
+            price="9.99"
+            title="Take My Money"
+            :perks="[
+              'Access to all basic features (yes you get dark theme too)',
+              '1000 AI tokens per month (enough to look busy at work)',
+            ]"
+            :button="{
+              text: 'Upgrade to Pro',
+              to: '/upgrade',
+              variant: 'default',
+            }"
+            description="More tokens,  more productivity."
+          />
+          <LandingPriceCard
+            onetime
+            price="19.99"
+            title="Token Junkie"
+            :perks="[
+              'Purchase 2000 AI tokens (because you can never have enough)',
+            ]"
+            :extras="['No pro subscription required (we\'re not that clingy)']"
+            :button="{
+              text: 'Buy Tokens',
+              to: '/buy-tokens',
+              variant: 'outline',
+            }"
+            description="Need more tokens? We got you."
+          />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
