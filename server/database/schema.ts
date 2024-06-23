32ab015d59f95d864e6a4d1d6c405797baf8c566
Kyng Cytro <johndibashi2001@gamil.com>
2024-06-23 13:22:31 +0100

    feat: add database and set up Drizzle

---
+++ server/database/schema.ts
@@ -0,0 +1,10 @@
+import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core";
+
+export const users = sqliteTable("users", {
+  id: integer("id").primaryKey({ autoIncrement: true }),
+  name: text("name").notNull(),
+  email: text("email").notNull().unique(),
+  password: text("password").notNull(),
+  avatar: text("avatar").notNull(),
+  createdAt: integer("created_at", { mode: "timestamp" }).notNull(),
+});
