a01566bef9d7d3f6bf7b9e51cb3e3af26ad381a8
Kyng Cytro <johndibashi2001@gamil.com>
2024-06-24 17:54:03 +0100

    feat: add error page and check-email page

---
+++ error.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { TriangleAlert } from "lucide-vue-next";
+import type { NuxtError } from "#app";
+const router = useRouter();
+defineProps({
+  error: Object as () => NuxtError,
+});
+</script>
+
+<template>
+  <div class="-mt-16">
+    <div class="flex h-screen items-center justify-center">
+      <div class="relative isolate w-full max-w-6xl px-6 py-14 lg:px-8">
+        <Card class="mx-auto max-w-sm">
+          <CardHeader>
+            <CardTitle class="text-xl font-semibold leading-8">
+              <TriangleAlert class="mb-2 h-8 w-8" />
+              <span>Something went wrong</span>
+            </CardTitle>
+            <CardDescription class="mt-1 text-[0.9rem]">
+              <p class="overflow-hidden">
+                {{ error?.statusCode }}
+                {{ error?.statusMessage || error?.message }}
+              </p>
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <Button class="mt-2" variant="outline" @click="router.back()">
+              Back Home
+            </Button>
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  </div>
+</template>
--- pages/login/index.vue
+++ pages/login/index.vue
@@ -23,74 +23,82 @@ const onSubmit = async (values: Record<string, any>) => {
 
 <template>
   <div class="-mt-16">
-    <Card class="mx-auto max-w-sm">
-      <CardHeader>
-        <CardTitle class="text-2xl font-semibold leading-8">
-          Lets Get You Started
-        </CardTitle>
-        <CardDescription class="mt-1">
-          Enter your email below to create your account
-        </CardDescription>
-      </CardHeader>
-      <CardContent>
-        <Form
-          class="grid gap-4"
-          :validation-schema="formSchema"
-          @submit="onSubmit"
-        >
-          <FormField name="email" v-slot="{ componentField }">
-            <FormItem>
-              <FormControl>
-                <Input
-                  type="email"
-                  placeholder="hello@example.com"
-                  v-bind="componentField"
-                />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          </FormField>
-          <Button type="submit" class="w-full font-semibold" :loading="loading">
-            Sign In with Email
-          </Button>
-          <div class="relative">
-            <div class="absolute inset-0 flex items-center">
-              <span class="w-full border-t" />
-            </div>
-            <div class="relative flex justify-center text-xs uppercase">
-              <span class="bg-background px-2 text-muted-foreground">
-                Or continue with
-              </span>
-            </div>
-          </div>
-          <Button
-            variant="outline"
-            type="button"
-            @click="signIn({ type: 'google' })"
-          >
-            Sign In with Google
-          </Button>
-        </Form>
-        <div class="mt-4">
-          <p class="px-8 text-center text-sm text-muted-foreground">
-            By clicking continue, you agree to our
-            <a
-              href="/terms"
-              class="underline underline-offset-4 hover:text-primary"
+    <div class="flex items-center justify-center">
+      <div class="relative isolate w-full max-w-6xl px-6 py-14 lg:px-8">
+        <Card class="mx-auto max-w-sm">
+          <CardHeader>
+            <CardTitle class="text-2xl font-semibold leading-8">
+              Lets Get You Started
+            </CardTitle>
+            <CardDescription class="mt-1">
+              Enter your email below to create your account
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <Form
+              class="grid gap-4"
+              :validation-schema="formSchema"
+              @submit="onSubmit"
             >
-              Terms of Service
-            </a>
-            and
-            <a
-              href="/privacy"
-              class="underline underline-offset-4 hover:text-primary"
-            >
-              Privacy Policy
-            </a>
-            .
-          </p>
-        </div>
-      </CardContent>
-    </Card>
+              <FormField name="email" v-slot="{ componentField }">
+                <FormItem>
+                  <FormControl>
+                    <Input
+                      type="email"
+                      placeholder="hello@example.com"
+                      v-bind="componentField"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              </FormField>
+              <Button
+                type="submit"
+                class="w-full font-semibold"
+                :loading="loading"
+              >
+                Sign In with Email
+              </Button>
+              <div class="relative">
+                <div class="absolute inset-0 flex items-center">
+                  <span class="w-full border-t" />
+                </div>
+                <div class="relative flex justify-center text-xs uppercase">
+                  <span class="bg-background px-2 text-muted-foreground">
+                    Or continue with
+                  </span>
+                </div>
+              </div>
+              <Button
+                variant="outline"
+                type="button"
+                @click="signIn({ type: 'google' })"
+              >
+                Sign In with Google
+              </Button>
+            </Form>
+            <div class="mt-4">
+              <p class="px-8 text-center text-sm text-muted-foreground">
+                By clicking continue, you agree to our
+                <a
+                  href="/terms"
+                  class="underline underline-offset-4 hover:text-primary"
+                >
+                  Terms of Service
+                </a>
+                and
+                <a
+                  href="/privacy"
+                  class="underline underline-offset-4 hover:text-primary"
+                >
+                  Privacy Policy
+                </a>
+                .
+              </p>
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+    </div>
   </div>
 </template>
