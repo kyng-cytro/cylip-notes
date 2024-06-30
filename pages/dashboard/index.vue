067bedc723663d17ea636e984ba8bdbdca5acd98
kyng-cytro <johndibashi2001@gmail.com>
2024-06-30 23:20:19 +0100

    refactor: move layout toggle to component

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,27 +1,14 @@
 <script setup lang="ts">
-import { LayoutGrid, StretchHorizontal } from "lucide-vue-next";
 definePageMeta({
   layout: "dashboard",
 });
-
-const layout = ref("grid");
 </script>
 
 <template>
   <main class="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
     <div class="flex items-center justify-between">
       <DashboardNoteGroupSelect />
-      <Button
-        variant="ghost"
-        size="icon"
-        @click="layout = layout === 'grid' ? 'stretch' : 'grid'"
-      >
-        <Transition name="fade" mode="out-in">
-          <StretchHorizontal v-if="layout === 'grid'" />
-          <LayoutGrid v-else />
-        </Transition>
-        <span class="sr-only">Toggle layout</span>
-      </Button>
+      <DashboardNoteLayoutSelect />
     </div>
     <DashboardNoteContainer />
     <!-- <DashboardEmptyPage -->
@@ -31,28 +18,3 @@ const layout = ref("grid");
     <!-- /> -->
   </main>
 </template>
-
-<style scoped>
-.fade-enter-active {
-  animation: scale-up-center 0.25s ease 0s 1 normal none;
-}
-.fade-leave-active {
-  animation: scale-down-center 0.25s ease 0s 1 normal none;
-}
-@keyframes scale-up-center {
-  0% {
-    transform: scale(0.2);
-  }
-  100% {
-    transform: scale(1);
-  }
-}
-@keyframes scale-down-center {
-  0% {
-    transform: scale(1);
-  }
-  100% {
-    transform: scale(0.2);
-  }
-}
-</style>
