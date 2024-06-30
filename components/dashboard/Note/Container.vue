c7b441e2a1af32248b903f896b65acf4e5883ee7
Kyng Cytro <johndibashi2001@gamil.com>
2024-06-30 23:19:37 +0100

    refactor: grid layout

--- components/dashboard/Note/Container.vue
+++ components/dashboard/Note/Container.vue
@@ -64,7 +64,7 @@ const notes = ref([
 
 <template>
   <VueDraggableNext
-    class="grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4"
+    class="grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5"
     v-bind="dragOptions"
     v-model="notes"
     @start="drag = true"
