0b5e12ddaeb63530a6c31cd85dadda55e5b9b65e
Kyng Cytro <johndibashi2001@gamil.com>
2024-07-01 15:44:29 +0100

    feat: add card actions

---
+++ components/TooltipWrapper.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+defineProps<{
+  tooltip: string;
+}>();
+</script>
+<template>
+  <TooltipProvider>
+    <Tooltip>
+      <TooltipTrigger as-child>
+        <slot />
+      </TooltipTrigger>
+      <TooltipContent>
+        <p>{{ tooltip }}</p>
+      </TooltipContent>
+    </Tooltip>
+  </TooltipProvider>
+</template>
--- components/dashboard/Note/index.vue
+++ components/dashboard/Note/index.vue
@@ -1,4 +1,11 @@
 <script setup lang="ts">
+import {
+  Plus,
+  BellRing,
+  Archive,
+  Palette,
+  EllipsisVertical,
+} from "lucide-vue-next";
 defineProps<{
   note: {
     id: number;
@@ -9,15 +16,84 @@ defineProps<{
 </script>
 <template>
   <Card
-    class="w-full cursor-pointer ring-blue-500 focus:outline-none focus:ring-2"
+    class="group w-full cursor-pointer ring-blue-500 focus:outline-none focus:ring-2"
     tabindex="0"
   >
     <CardHeader>
       <CardTitle>{{ note.title }}</CardTitle>
       <CardDescription>{{ note.content }}</CardDescription>
     </CardHeader>
-    <CardContent>
+    <CardContent class="pb-2.5">
       <Skeleton class="h-32 w-full" />
     </CardContent>
+    <CardFooter
+      class="invisible flex items-center justify-between pb-2.5 text-muted-foreground group-hover:visible group-focus:visible"
+    >
+      <TooltipWrapper tooltip="Remind me later">
+        <Button variant="ghost" size="icon">
+          <BellRing class="h-4 w-4" />
+        </Button>
+      </TooltipWrapper>
+      <TooltipWrapper tooltip="Archive">
+        <Button variant="ghost" size="icon">
+          <Archive class="h-4 w-4" />
+        </Button>
+      </TooltipWrapper>
+      <Badge variant="secondary">
+        <span>All Notes</span>
+      </Badge>
+      <TooltipWrapper tooltip="Change color">
+        <Button variant="ghost" size="icon">
+          <Palette class="h-4 w-4" />
+        </Button>
+      </TooltipWrapper>
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <Button variant="ghost" size="icon">
+            <EllipsisVertical class="h-4 w-4" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent class="w-[200px]">
+          <DropdownMenuLabel>Actions</DropdownMenuLabel>
+          <DropdownMenuGroup>
+            <DropdownMenuItem>Make a Copy</DropdownMenuItem>
+            <DropdownMenuItem>Version History</DropdownMenuItem>
+          </DropdownMenuGroup>
+          <DropdownMenuSeparator />
+          <DropdownMenuSub>
+            <DropdownMenuSubTrigger> Assign Group </DropdownMenuSubTrigger>
+            <DropdownMenuSubContent class="p-0">
+              <Command>
+                <CommandInput placeholder="Filter group..." auto-focus />
+                <CommandList>
+                  <CommandEmpty
+                    class="flex flex-col items-center justify-center gap-2"
+                  >
+                    <span>No group with that name.</span>
+                    <TooltipWrapper tooltip="Create a group with this name">
+                      <Button variant="ghost" size="xs">
+                        <Plus class="h-5 w-5" />
+                        <span class="sr-only"
+                          >Create a group with this name</span
+                        >
+                      </Button>
+                    </TooltipWrapper>
+                  </CommandEmpty>
+                  <CommandGroup>
+                    <CommandItem value="all-notes">All Notes</CommandItem>
+                  </CommandGroup>
+                </CommandList>
+              </Command>
+            </DropdownMenuSubContent>
+          </DropdownMenuSub>
+          <DropdownMenuSeparator />
+          <DropdownMenuGroup>
+            <DropdownMenuItem class="text-red-600"
+              >Delete Note</DropdownMenuItem
+            >
+          </DropdownMenuGroup>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </CardFooter>
   </Card>
 </template>
