e1c8c18a6e0d8a44ed01550d495c1e9db1e8c051
Kyng Cytro <johndibashi2001@gamil.com>
2024-07-01 16:49:49 +0100

    fix: make dropdown client-only

--- components/dashboard/Note/index.vue
+++ components/dashboard/Note/index.vue
@@ -44,53 +44,60 @@ defineProps<{
           <Palette class="h-4 w-4" />
         </Button>
       </TooltipWrapper>
-      <DropdownMenu>
-        <DropdownMenuTrigger>
+      <ClientOnly>
+        <template #fallback>
           <Button variant="ghost" size="icon">
             <EllipsisVertical class="h-4 w-4" />
           </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent class="w-[200px]">
-          <DropdownMenuLabel>Actions</DropdownMenuLabel>
-          <DropdownMenuGroup>
-            <DropdownMenuItem>Make a Copy</DropdownMenuItem>
-            <DropdownMenuItem>Version History</DropdownMenuItem>
-          </DropdownMenuGroup>
-          <DropdownMenuSeparator />
-          <DropdownMenuSub>
-            <DropdownMenuSubTrigger> Assign Group </DropdownMenuSubTrigger>
-            <DropdownMenuSubContent class="p-0">
-              <Command>
-                <CommandInput placeholder="Filter group..." auto-focus />
-                <CommandList>
-                  <CommandEmpty
-                    class="flex flex-col items-center justify-center gap-2"
-                  >
-                    <span>No group with that name.</span>
-                    <TooltipWrapper tooltip="Create a group with this name">
-                      <Button variant="ghost" size="xs">
-                        <Plus class="h-5 w-5" />
-                        <span class="sr-only"
-                          >Create a group with this name</span
-                        >
-                      </Button>
-                    </TooltipWrapper>
-                  </CommandEmpty>
-                  <CommandGroup>
-                    <CommandItem value="all-notes">All Notes</CommandItem>
-                  </CommandGroup>
-                </CommandList>
-              </Command>
-            </DropdownMenuSubContent>
-          </DropdownMenuSub>
-          <DropdownMenuSeparator />
-          <DropdownMenuGroup>
-            <DropdownMenuItem class="text-red-600"
-              >Delete Note</DropdownMenuItem
-            >
-          </DropdownMenuGroup>
-        </DropdownMenuContent>
-      </DropdownMenu>
+        </template>
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <Button variant="ghost" size="icon">
+              <EllipsisVertical class="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent class="w-[200px]">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuGroup>
+              <DropdownMenuItem>Make a Copy</DropdownMenuItem>
+              <DropdownMenuItem>Version History</DropdownMenuItem>
+            </DropdownMenuGroup>
+            <DropdownMenuSeparator />
+            <DropdownMenuSub>
+              <DropdownMenuSubTrigger> Assign Group </DropdownMenuSubTrigger>
+              <DropdownMenuSubContent class="p-0">
+                <Command>
+                  <CommandInput placeholder="Filter group..." auto-focus />
+                  <CommandList>
+                    <CommandEmpty
+                      class="flex flex-col items-center justify-center gap-2"
+                    >
+                      <span>No group with that name.</span>
+                      <TooltipWrapper tooltip="Create a group with this name">
+                        <Button variant="ghost" size="xs">
+                          <Plus class="h-5 w-5" />
+                          <span class="sr-only"
+                            >Create a group with this name</span
+                          >
+                        </Button>
+                      </TooltipWrapper>
+                    </CommandEmpty>
+                    <CommandGroup>
+                      <CommandItem value="all-notes">All Notes</CommandItem>
+                    </CommandGroup>
+                  </CommandList>
+                </Command>
+              </DropdownMenuSubContent>
+            </DropdownMenuSub>
+            <DropdownMenuSeparator />
+            <DropdownMenuGroup>
+              <DropdownMenuItem class="text-red-600"
+                >Delete Note</DropdownMenuItem
+              >
+            </DropdownMenuGroup>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </ClientOnly>
     </CardFooter>
   </Card>
 </template>
