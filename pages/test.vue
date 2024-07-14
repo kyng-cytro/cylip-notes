ef919eec14a757b40b2dabc143153d72137c3a0f
kyng-cytro <johndibashi2001@gmail.com>
2024-07-14 02:00:45 +0100

    fix: test regular SSE implementation

---
+++ pages/test.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const { baseUrl } = useRuntimeConfig().public;
+const { data } = useEventSource(`${baseUrl}/sse2`);
+</script>
+<template>
+  <div>
+    {{ data }}
+  </div>
+</template>
