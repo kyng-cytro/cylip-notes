9da3e6245a13de4ac7034ea5bf5065b8d7e774b2
kyng-cytro <johndibashi2001@gmail.com>
2024-07-14 02:13:55 +0100

    fix: test new version of hooked SSE

--- server/routes/sse2.ts
+++ server/routes/sse2.ts
@@ -1,14 +1,15 @@
 export default defineEventHandler(async (event) => {
   const eventStream = createEventStream(event);
 
-  const interval = setInterval(async () => {
-    await eventStream.push(`Message @ ${new Date().toLocaleTimeString()}`);
-  }, 1000);
+  const sendEvent = (data: any) => {
+    eventStream.push(JSON.stringify(data));
+  };
 
   eventStream.onClosed(async () => {
-    clearInterval(interval);
     await eventStream.close();
   });
+
+  sseHooks.hook(`sse:event:1`, sendEvent);
 
   return eventStream.send();
 });
