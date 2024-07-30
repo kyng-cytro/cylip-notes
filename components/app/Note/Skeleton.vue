9dbb94d4e2103b057458c64d653b05e07e9abdb9
kyng-cytro <johndibashi2001@gmail.com>
2024-07-30 19:53:44 +0100

    fix: update loading skeleton to use new notes layout

--- components/app/Note/Skeleton.vue
+++ components/app/Note/Skeleton.vue
@@ -1,10 +1,25 @@
+<script setup lang="ts">
+const { layout } = storeToRefs(useLayoutStore());
+defineProps<{
+  preview?: boolean;
+}>();
+</script>
+
 <template>
-  <Card class="w-full">
-    <CardHeader>
+  <Card
+    class="flex w-full max-w-sm flex-1 flex-col gap-3 px-3 py-2"
+    :class="{
+      'max-w-none self-start': preview,
+      'min-w-32 md:min-w-[300px]': layout === 'grid',
+      'max-w-none': layout === 'list',
+    }"
+  >
+    <Skeleton class="h-32 w-full" v-if="preview" />
+    <div
+      class="flex items-center justify-between rounded-b-lg"
+      :class="{ 'rounded-t-lg': preview }"
+    >
       <Skeleton class="h-10 w-full" />
-    </CardHeader>
-    <CardContent>
-      <Skeleton class="h-32 w-full" />
-    </CardContent>
+    </div>
   </Card>
 </template>
--- components/app/NotesLoading.vue
+++ components/app/NotesLoading.vue
@@ -1,20 +1,14 @@
 <script setup lang="ts">
 const { layout } = storeToRefs(useLayoutStore());
-const count = 4;
 const layoutStyles = computed(() => ({
-  "grid gap-4 grid-cols-2": layout.value === "grid",
-  "sm:grid-cols-[repeat(auto-fit,minmax(200px,max-content))]":
-    layout.value === "grid" && count < 3,
-  "sm:grid-cols-[repeat(auto-fit,minmax(200px,1fr))] lg:grid-cols-[repeat(auto-fit,minmax(200px,0.15fr))]":
-    layout.value === "grid" && count >= 3 && count < 5,
-  "sm:grid-cols-[repeat(auto-fit,minmax(200px,1fr))] lg:grid-cols-[repeat(auto-fit,minmax(200px,1fr))]":
-    layout.value === "grid" && count >= 5,
-  "grid-cols-1": layout.value === "list",
+  "flex-wrap": layout.value === "grid",
+  "flex-col": layout.value === "list",
 }));
 </script>
 
 <template>
-  <div class="grid gap-4" :class="layoutStyles">
-    <AppNoteSkeleton v-for="i in count" :key="i" />
+  <div class="flex gap-4" :class="layoutStyles">
+    <AppNoteSkeleton v-for="i in 3" :key="i" />
+    <AppNoteSkeleton v-for="i in 3" :key="i" preview />
   </div>
 </template>
