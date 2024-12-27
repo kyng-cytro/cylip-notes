29fa15b1d34f504d1aa38567c0a43d0b3f4e48e8
kyng-cytro <johndibashi2001@gmail.com>
2024-12-27 14:28:02 +0100

    chore(ui): better loading animation

--- components/app/NotesLoading.vue
+++ components/app/NotesLoading.vue
@@ -1,9 +1,17 @@
 <script setup lang="ts">
 const { conatinerStyles: layoutStyles } = useLayout();
+const heights = [
+  50, 50, 150, 300, 200, 100, 250, 350, 150, 100, 400, 300, 250, 200, 50,
+];
 </script>
 
 <template>
-  <div class="flex gap-4" :class="layoutStyles">
-    <AppNoteSkeleton v-for="i in 5" :key="i" preview />
+  <div :class="layoutStyles">
+    <Skeleton
+      v-for="i in 20"
+      :key="i"
+      class="mb-2 w-full break-inside-avoid"
+      :style="{ height: `${heights[(i - 1) % heights.length]}px` }"
+    />
   </div>
 </template>
