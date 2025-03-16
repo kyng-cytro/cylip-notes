b646ebeab3be9f80bda145c3f8fd9129d45f1d67
kyng-cytro <johndibashi2001@gmail.com>
2025-03-16 13:38:48 +0100

    chore: fix hidden scrollbar

--- composables/layout.ts
+++ composables/layout.ts
@@ -2,10 +2,8 @@ export const useLayout = () => {
   const { layout } = storeToRefs(useLayoutStore());
 
   const containerParentStyles = computed(() => ({
-    "scrollbar-thin lg:scrollbar-none hover:scrollbar-thin pr-2 ":
-      layout.value === "grid",
-    "scrollbar-thin w-full max-w-xl mx-auto lg:scrollbar-none hover:scrollbar-thin pr-2 ":
-      layout.value === "list",
+    "scrollbar-thin pr-2 ": layout.value === "grid",
+    "scrollbar-thin w-full max-w-xl mx-auto pr-2 ": layout.value === "list",
   }));
 
   const conatinerStyles = computed(() => ({
