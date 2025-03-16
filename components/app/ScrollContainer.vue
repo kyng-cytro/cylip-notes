8729fe273c0470f2c66b42d3b7e1f266f44946c1
kyng-cytro <johndibashi2001@gmail.com>
2025-03-16 13:49:22 +0100

    fix: finally fix scroll container issue

--- components/app/ScrollContainer.vue
+++ components/app/ScrollContainer.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="flex max-h-[calc(100vh-160px)] flex-col gap-4 overflow-y-auto overflow-x-hidden p-1"
+    class="flex max-h-[calc(100vh-170px)] flex-col gap-4 overflow-y-auto overflow-x-hidden p-1"
   >
     <slot />
   </div>
