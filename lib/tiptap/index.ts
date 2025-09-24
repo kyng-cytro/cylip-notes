9bd15f94201d0bc4fb1705313de0bbf819daa775
kyng-cytro <johndibashi2001@gmail.com>
2025-09-24 15:08:59 +0100

    chore: re-add node range

--- lib/tiptap/index.ts
+++ lib/tiptap/index.ts
@@ -2,6 +2,7 @@ import lowlight from "@/lib/lowlight";
 import { CodeBlock, MarkDownCopy } from "@/lib/tiptap/custom-extensions";
 import Highlight from "@tiptap/extension-highlight";
 import Image from "@tiptap/extension-image";
+import NodeRange from "@tiptap/extension-node-range";
 import Placeholder from "@tiptap/extension-placeholder";
 import TaskItem from "@tiptap/extension-task-item";
 import TaskList from "@tiptap/extension-task-list";
@@ -31,5 +32,8 @@ export const extensions = [
     },
     heading: { levels: [4] },
     horizontalRule: { HTMLAttributes: { class: "bg-primary border-1" } },
+  }),
+  NodeRange.configure({
+    key: null,
   }),
 ];
